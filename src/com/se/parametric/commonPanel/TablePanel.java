4c5eeff097da90c19e91e8ec5c0c9689ed20ae9b
hanan_sabry <hanan_sabry@siliconexpert.com>
2014-11-17 16:49:07 +0200

    aa

--- src/com/se/parametric/commonPanel/TablePanel.java
+++ src/com/se/parametric/commonPanel/TablePanel.java
@@ -349,8 +349,13 @@ public class TablePanel extends JPanel implements ActionListener
 	}
 	
 	public void updateSheetPanelPagging(){
+		System.err.println(getCurrentPage());	
 		setPaggingLabel(getCurrentPage());
-		setTableData1(1, getRows1(getCurrentPage()));
+//		try{
+			setTableData1(1, getRows1(getCurrentPage()));
+//		}catch(Exception ex){
+//			
+//		}
 	}
 	
 	public void setTableData(int x, ArrayList<ArrayList<String>> result)
