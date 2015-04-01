16c49d7781345f99152fbf65e3f176fccb1f627b
Mohamed_Elshabrawy <Mohamed_Elshabrawy@mohamed_elshabrawy.SiliconEgypt.com>
2015-04-01 17:21:27 +0200

    factory pattern

--- src/com/se/users/action/newtype/approved/Eng.java
+++ src/com/se/users/action/newtype/approved/Eng.java
@@ -1,11 +1,5 @@
 package com.se.users.action.newtype.approved;
 
-import java.util.ArrayList;
-
-import javax.swing.JToolBar;
-
-import com.se.parametric.dto.GrmUserDTO;
-
 public class Eng extends Approved{
 
 	
